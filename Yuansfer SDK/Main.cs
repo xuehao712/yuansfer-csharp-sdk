d516ec1ff2855bbc74f0382533e67974275c6bd4
Xuehao Zheng <xuehao712@gmail.com>
2021-01-18 00:34:17 -0500

    Add Main package setting

---
+++ Yuansfer SDK/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuansfer_SDK
+{
+    public class Main
+    {
+        static void main(string[] args)
+        {
+            
+            
+        }
+    }
+}
